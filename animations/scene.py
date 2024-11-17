50851e9f6077ac72df332422aec378d583c2e890
Matt Smith <msmith21794@gmail.com>
2024-11-17 13:01:57 -0500

    updated tracked files and added conc graph

--- animations/scene.py
+++ animations/scene.py
@@ -20,6 +20,11 @@ def extract_simulation_concs(file_path):
     return data_dict
 
 class CompartmentalModel(Scene):
+    def conc_func_generator(self, time, conc):
+        def conc_func(self, t):
+            return (time[t], conc[t], 0)
+        return conc_func
+    
     def construct(self):
         # Compartments
         circle_depot = Circle(color = GREEN).shift(3.8 * LEFT)
@@ -75,36 +80,61 @@ class CompartmentalModel(Scene):
 
         self.wait(1)
 
-        self.play(
-            Create(arrows[4]), Create(arrow_labels[4])
-        )
-
         sim_results = extract_simulation_concs("simulated_results/simulation_results.csv")
-        max_depot = max(concs[0] for concs in sim_results.values())
-        max_central = max(concs[1] for concs in sim_results.values())
-        max_periph = max(concs[2] for concs in sim_results.values())
+        time = list(sim_results.keys())
+        depot_concs = [concs[0] for concs in sim_results.values()]
+        central_concs = [concs[1] for concs in sim_results.values()]
+        periph_concs = [concs[2] for concs in sim_results.values()]
         
-        max_conc = max(max_depot, max_central, max_periph)
-        
-        total_duration = 20
+        max_conc = max(max(depot_concs), max(central_concs), max(periph_concs))
+        # Create fill animation
+        total_duration = 12
         num_frames = len(sim_results)
         frame_duration = total_duration / num_frames
-        # Create animations for each time step
-        animations = []
-        for i, (t, concs) in enumerate(sim_results.items()):
-            if i == 0:
-                self.play(
-                    FadeOut(arrows[4]),
-                    FadeOut(arrow_labels[4])
-                )
+        
+        # Create graphs
+        axes = Axes(
+            x_range=[0, max(time), 24],
+            y_range=[0, max_conc, max_conc / 5],
+            x_length=4,
+            y_length=2,
+            axis_config={"include_tip": False}
+        ).to_edge(UP).shift(0.25 * UP)
 
+        depot_graph = axes.plot_line_graph(time, depot_concs, line_color=GREEN, vertex_dot_radius=0)
+        central_graph = axes.plot_line_graph(time, central_concs, line_color=PINK, vertex_dot_radius=0)
+        periph_graph = axes.plot_line_graph(time, periph_concs, line_color=BLUE, vertex_dot_radius=0)
+
+        depot_dot = Dot(color=GREEN).move_to(axes.c2p(time[0], depot_concs[0]))
+        central_dot = Dot(color=PINK).move_to(axes.c2p(time[0], central_concs[0]))
+        periph_dot = Dot(color=BLUE).move_to(axes.c2p(time[0], periph_concs[0]))
+
+        animations = []
+        for i, (time, concs) in enumerate(sim_results.items()):
+            
             animations.append(
                 AnimationGroup(
                     circle_depot.animate.set_fill(GREEN, opacity=concs[0]/max_conc),
                     circle_central.animate.set_fill(PINK, opacity=concs[1]/max_conc),
                     circle_periph.animate.set_fill(BLUE, opacity=concs[2]/max_conc),
+
+                    depot_dot.animate.move_to(axes.c2p(time, concs[0])),
+                    central_dot.animate.move_to(axes.c2p(time, concs[1])),
+                    periph_dot.animate.move_to(axes.c2p(time, concs[2])),
+
                     lag_ratio=0,  # Synchronous updates
                     run_time=frame_duration,
                 )
             )
-        self.play(Succession(*animations))
+
+        self.play(
+            Create(arrows[4]), Create(arrow_labels[4])
+        )
+        
+        self.add(axes)
+        self.add(depot_graph, central_graph, periph_graph)
+
+        self.play(
+            #FadeOut(arrows[4]), FadeOut(arrow_labels[4]),
+            Succession(*animations)
+        )
